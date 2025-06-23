f7d08bfc3800de9acc65f00d3a6c66fbc7d0a7d9
Ayush Pratap Singh <ayushprataps135@gmail.com>
2025-06-23 19:05:37 +0530

    Implemented user auth flow using Express-Sessions (signup, login, profile, logout)

---
+++ Backend/Express-sessions/app.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const app = express();
+const PORT = 4444;
+const session = require('express-session');
+
+app.set('view engine','hbs');
+app.use(express.urlencoded({extended:true}));
+
+app.use(session({
+    secret: 'randomid',
+    resave: false,
+    saveUninitialized: true,
+}));
+
+app.get('/',(req,res)=>{
+    res.redirect('/login')
+})
+
+app.get('/signup',(req,res)=>{
+    res.render('signup');
+
+})
+
+app.post('/signup',(req,res)=>{
+    const {username, password} = req.body;
+    req.session.username = username;
+    req.session.password = password;
+
+    // console.log(req.session);
+    res.redirect('login');
+})
+
+app.get('/login',(req,res)=>{
+    res.render('login')
+})
+
+app.post('/login',(req,res)=>{
+    const {username, password} = req.body;
+    if(username === req.session.username && password === req.session.password)
+        return res.redirect('/profile');
+    res.send('Login failed');
+})
+
+app.get('/profile',(req,res)=>{
+    if(req.session.username){
+        res.render('profile',{
+            username: req.session.username,
+            password: req.session.password
+        });
+    }
+    else res.redirect('/login');
+})
+
+app.get('/logout',(req,res)=>{
+    req.session.destroy();
+    res.redirect('/');
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server started at http://localhost:${PORT}`);
+})
