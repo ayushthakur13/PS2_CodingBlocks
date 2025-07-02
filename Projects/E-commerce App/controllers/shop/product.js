a1472ed92a34cc53e467be3dcbe0af05d845bfc1
Ayush Pratap Singh <ayushprataps135@gmail.com>
2025-07-02 23:59:33 +0530

    E-commerce app completed — finished full cart system with quantity updates, delete, UI, and total price.

--- Projects/E-commerce App/controllers/shop/product.js
+++ Projects/E-commerce App/controllers/shop/product.js
@@ -57,3 +57,112 @@ module.exports.getAddToCart = async (req, res, next) => {
         res.status(500).json({ error: 'Something went wrong!' });
     }
 };
+
+//function to count total price
+function findTotalPrice(products){
+    let totalPrice = 0;
+    products.forEach(element => {
+        let price = element.id.price * element.quantity;
+        totalPrice += price;
+    });
+    return totalPrice;
+}
+
+
+module.exports.getCart = async (req,res,next)=>{
+    try{
+        req.user.populate('cart.products.id')
+            .then((user)=>{
+                let products = user.cart.products;
+
+                let totalPrice = findTotalPrice(products);
+
+                res.render('shop/cart',{
+                    products,
+                    totalPrice
+                });
+            })
+    }
+    catch(e){
+        next(e);
+    }
+}
+
+
+module.exports.getIncreaseQuantity = async (req, res, next) => {
+    const { productId } = req.query;
+
+    try {
+        await req.user.populate('cart.products.id');
+
+        let products = req.user.cart.products;
+
+
+        for (let element of products) {
+            if (element.id._id.equals(productId)) {
+                element.quantity += 1;
+
+                let totalPrice = findTotalPrice(products);
+
+                await req.user.save();
+                return res.send({ quantity: element.quantity, totalPrice });
+            }
+        }
+
+        return res.json({ message: 'Product not found in cart' });
+
+    } 
+    catch (e) {
+        next(e);
+    }
+};
+
+
+module.exports.getDecreaseQuantity = async (req, res, next) => {
+    const { productId } = req.query;
+
+    try {
+        await req.user.populate('cart.products.id');
+
+        let products = req.user.cart.products;
+
+        for (let element of products) {
+            if (element.id._id.equals(productId)) {
+                if (element.quantity > 1) element.quantity -= 1;
+
+                let totalPrice = findTotalPrice(products);
+
+                await req.user.save();
+                return res.send({ quantity: element.quantity, totalPrice });
+            }
+        }
+
+        return res.json({ message: 'Product not found in cart' });
+
+    } 
+    catch (e) {
+        next(e);
+    }
+};
+
+
+module.exports.getDeleteCartItem = async (req, res, next) => {
+    const { productId } = req.query;
+
+    try {
+        await req.user.populate('cart.products.id');
+
+        req.user.cart.products = req.user.cart.products.filter(p => {
+            return !p.id._id.equals(productId);
+        });
+
+        await req.user.save();
+
+        const totalPrice = findTotalPrice(req.user.cart.products);
+
+        return res.send({ totalPrice });
+    } 
+    catch (e) {
+        next(e);
+    }
+};
--- Projects/E-commerce App/routes/shop.js
+++ Projects/E-commerce App/routes/shop.js
@@ -7,5 +7,9 @@ router.get('/products',shopController.getProducts);
 router.get('/details',shopController.getDetails);
 router.post('/submitreview',shopController.postSubmitReview);
 router.get('/addtocart',shopController.getAddToCart);
+router.get('/cart',shopController.getCart);
+router.get('/increasequantity',shopController.getIncreaseQuantity);
+router.get('/decreasequantity',shopController.getDecreaseQuantity);
+router.get('/deletecartitem',shopController.getDeleteCartItem);
 
 module.exports = router;
